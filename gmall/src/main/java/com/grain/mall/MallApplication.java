226f57380ebe55ea4f7bdb93602e95dd33e66538
nice-gongjin <1678541105@qq.com>
2019-12-14 23:17:18 +0800

    t添加了配置文件

--- gmall/src/main/java/com/grain/mall/MallApplication.java
+++ gmall/src/main/java/com/grain/mall/MallApplication.java
@@ -1,9 +1,11 @@
 package com.grain.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.grain.mall.mapper") // 扫描 Mapper 文件夹 【注：根据自己的项目结构配置】
 public class MallApplication {
 
     public static void main(String[] args) {
